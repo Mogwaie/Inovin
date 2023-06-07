de5cc1d72ddde0dfad25af88a7f4639e2d8f1001
marine <marinou.boulanger@gmail.com>
2023-06-07 11:37:38 +0200

    commit navbar user  before pull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import Home from "./pages/Home";
 import "./main.scss";
+import Navbar from "./components/NavbarUser";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
-      <p>coucou</p>
     </div>
   );
 }
