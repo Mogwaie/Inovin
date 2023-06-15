60df64320eb2938784e872c8bc58234fdc8b32ce
OLESIA TUR <olesia.tur@gmail.com>
2023-06-15 14:16:18 +0200

    backend routes for cepages done

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const cepageControllers = require("./controllers/cepageControllers");
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+router.get("/cepages", cepageControllers.findAllCepages);
+router.get("/cepages/:id", cepageControllers.findCepageById);
+router.post("/cepages", cepageControllers.createNewCepage);
+router.put("/cepages/:id", cepageControllers.editCepage);
+router.delete("/cepages/:id", cepageControllers.deleteCepage);
 
 module.exports = router;
