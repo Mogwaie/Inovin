489ad3236595dc0f9c4785e8031f85b276ed1a3b
EricJKL33 <eric.jkl@icloud.com>
2023-06-15 11:03:13 +0200

    taste routre

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,11 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const tasteControllers = require("./controllers/tasteControllers");
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+router.get("/taste", tasteControllers.tasteBrowse);
+router.get("/taste/:id", tasteControllers.read);
+router.put("/taste/:id", tasteControllers.edit);
+router.post("/taste", tasteControllers.add);
+router.delete("/taste/:id", tasteControllers.destroy);
 
 module.exports = router;
