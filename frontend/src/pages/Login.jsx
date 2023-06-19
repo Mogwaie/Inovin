bee2f88f36f2f3a7f91515280b8609e5f4ae84d3
marine <marinou.boulanger@gmail.com>
2023-06-19 17:07:39 +0200

    commut before pull

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,11 +3,13 @@ export default function Login() {
     <>
       <h2>Accéder à notre compte</h2>
       <form>
-        <input type="text" value="email*" />
-        <input type="text" value="Mot de passe*" />
+        <button className="primary-button" type="submit">
+          Connexion
+        </button>
       </form>
-      <button type="button">Connexion</button>
-      <button type="button">Inscription</button>
+      <button className="primary-button" type="button">
+        Inscription
+      </button>
       <p>Mot de passe oublié ?</p>
     </>
   );
