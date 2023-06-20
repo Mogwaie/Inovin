69901b468612c063de9b10251f922c2271a6fb3b
marine <marinou.boulanger@gmail.com>
2023-06-20 13:56:04 +0200

    commit before pull

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,16 +1,25 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
   return (
-    <>
+    <div className="login-container-page">
       <h2>Accéder à notre compte</h2>
       <form>
-        <button className="primary-button" type="submit">
-          Connexion
-        </button>
+        <input type="email" required value="email" />
+        <Link to="/change to next page">
+          <button className="primary-button" type="submit">
+            Connexion
+          </button>
+        </Link>
       </form>
-      <button className="primary-button" type="button">
-        Inscription
-      </button>
-      <p>Mot de passe oublié ?</p>
-    </>
+      <Link to="/signUp">
+        <button className="primary-button" type="button">
+          Inscription
+        </button>
+      </Link>
+      <Link to="/passwordRecovery">
+        <p>Mot de passe oublié ?</p>
+      </Link>
+    </div>
   );
 }
