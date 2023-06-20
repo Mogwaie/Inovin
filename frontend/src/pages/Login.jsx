109c1fb7ca06f27b2376b51a7d0beddb83683fbf
marine <marinou.boulanger@gmail.com>
2023-06-20 17:02:24 +0200

    commit before pull

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
+import InputPassword from "../components/InputPassword";
+import InputEmail from "../components/InputEmail";
 
 export default function Login() {
   return (
     <div className="login-container-page">
       <h2>Accéder à notre compte</h2>
+
       <form>
-        <input type="email" required value="email" />
+        <InputEmail />
+        <InputPassword />
         <Link to="/change to next page">
           <button className="primary-button" type="submit">
             Connexion
           </button>
         </Link>
       </form>
+
       <Link to="/signUp">
         <button className="primary-button" type="button">
           Inscription
