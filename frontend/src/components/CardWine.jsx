d3b60e8377aa2989889df6e68646ce4a37152452
marine <marinou.boulanger@gmail.com>
2023-07-03 14:03:06 +0200

    card component without props

--- frontend/src/components/CardWine.jsx
+++ frontend/src/components/CardWine.jsx
@@ -1,15 +1,16 @@
-// import wineGlass from "../assets/images/wineGlass.png";
+import wineBottle from "../assets/images/wine-bottle.svg";
 
 export default function CardWine() {
   return (
     <div className="card-container">
-      {/* <img src={wineGlass} alt="wine bottle" />
-      <div className="body-wineArray">
-        <h2>Wine 1</h2>
+      <img src={wineBottle} alt="wine bottle to buy" />
+      <div className="card-body-wine-description-and-title">
+        <h4>Wine 1</h4>
         <p>
-          blabblablablablablablablablablablablablablbalalalbalbalbalbalbalallabozbibou
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry.
         </p>
-      </div> */}
+      </div>
     </div>
   );
 }
