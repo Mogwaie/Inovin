a0e29be5ab4f7454a5805c99fde8983d3d577382
marine <marinou.boulanger@gmail.com>
2023-07-05 11:28:10 +0200

    switch

---
+++ frontend/src/components/admin/SliderAdmin.jsx
@@ -0,0 +1,9 @@
+function SliderAdmin() {
+  return (
+    <label className="switch">
+      <input type="checkbox" id="toggle" />
+      <span className="slider-round" />
+    </label>
+  );
+}
+export default SliderAdmin;
