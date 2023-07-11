e09418236dd6ac3c3aeb8dc2ab7c55068ca400b4
Cyril Freeman <cyril.freeman@hotmail.fr>
2023-07-11 09:32:00 +0200

    pop up is ready

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./main.scss";
+import { ToastContainer } from "react-toastify";
 import Root from "./routes/Root";
 
 import MaxLengthProvider from "./context/MaxLengthContext";
@@ -21,6 +22,7 @@ import DegustationProfile from "./pages/DegustationProfile";
 import WineList from "./pages/admin/WineList";
 import DegustationProfil from "./pages/admin/DegustationProfil";
 import PasswordResetForm from "./pages/PasswordResetForm";
+import ForgottenPassword from "./pages/ForgottenPassword";
 
 const router = createBrowserRouter([
   {
@@ -87,6 +89,10 @@ const router = createBrowserRouter([
         path: "/passwordresetform",
         element: <PasswordResetForm />,
       },
+      {
+        path: "/forgottenpasswordform",
+        element: <ForgottenPassword />,
+      },
     ],
   },
 ]);
@@ -96,6 +102,7 @@ root.render(
   <React.StrictMode>
     <MaxLengthProvider>
       <RouterProvider router={router} />
+      <ToastContainer closeButton={false} />
     </MaxLengthProvider>
   </React.StrictMode>
 );
