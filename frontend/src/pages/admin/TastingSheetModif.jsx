a7b3acfdc7ae39676dd25286b5a963d1b4c074b4
marine <marinou.boulanger@gmail.com>
2023-07-11 11:16:37 +0200

    fix tasting sheet

--- frontend/src/pages/admin/TastingSheetModif.jsx
+++ frontend/src/pages/admin/TastingSheetModif.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 import SliderUser from "../../components/SliderUser";
 import Button from "../../components/Button";
 
@@ -9,35 +10,23 @@ function TastingSheetModif() {
   const navigateToUserListPage = async () => {
     navigateTo("/user-list");
   };
+
+  const [wineListTasting, setWineListTasting] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:4242/api/tastes`).then((response) => {
+      setWineListTasting(response.data);
+    });
+  }, []);
+
   return (
     <div className="tasting-sheet-admin-conatainer ">
       <h2>Fiche dégustation</h2>
       <h3>Quels sont vos goûts ?</h3>
       <div className="slider-degustation-wine">
-        <div className="taste-slider">
-          <SliderUser tasteName="Châteauneuf-du-Pape" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Château Margaux" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Beaujolais Nouveau" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Domaine de la Romanée" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Châteauneuf-du-Pape" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Château Margaux" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Beaujolais Nouveau" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Domaine de la Romanée" />
-        </div>
+        {wineListTasting.map((wine) => (
+          <SliderUser key={wine.taste_id} tasteName={wine.name} />
+        ))}
       </div>
       <div className="button-end-tasting">
         <Button
