baaefefe1b0ca988b71e4e2e20a17c28e6e11773
marine <marinou.boulanger@gmail.com>
2023-07-11 10:57:03 +0200

    commit before change branch

--- frontend/src/pages/admin/TastingSheetModif.jsx
+++ frontend/src/pages/admin/TastingSheetModif.jsx
@@ -9,35 +9,26 @@ function TastingSheetModif() {
   const navigateToUserListPage = async () => {
     navigateTo("/user-list");
   };
+
+  const wineListTasting = [
+    "Châteauneuf-du-Pape",
+    "Château Margaux",
+    "Beaujolais Nouveau",
+    "Domaine de la Romanée",
+    "Le Sancerre Blanc",
+    "Pouilly-Fuissé",
+    "Châteauneuf-du-Pape Blanc",
+    "Château Yquem",
+  ];
+
   return (
     <div className="tasting-sheet-admin-conatainer ">
       <h2>Fiche dégustation</h2>
       <h3>Quels sont vos goûts ?</h3>
       <div className="slider-degustation-wine">
-        <div className="taste-slider">
-          <SliderUser tasteName="Châteauneuf-du-Pape" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Château Margaux" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Beaujolais Nouveau" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Domaine de la Romanée" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Châteauneuf-du-Pape" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Château Margaux" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Beaujolais Nouveau" />
-        </div>
-        <div className="taste-slider">
-          <SliderUser tasteName="Domaine de la Romanée" />
-        </div>
+        {wineListTasting.map((wine) => (
+          <SliderUser key={wine} tasteName={wine} />
+        ))}
       </div>
       <div className="button-end-tasting">
         <Button
