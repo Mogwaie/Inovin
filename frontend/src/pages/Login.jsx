eb872106f4b98333b92d1fa0d80a5abc74d0e05c
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-12 14:31:57 +0200

    Login - Replace the URL of Link for password recovery

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -67,7 +67,7 @@ export default function Login() {
           </Link>
         </div>
 
-        <Link to="/passwordresetform">
+        <Link to="/password-reset-form">
           <p>Mot de passe oublié ?</p>
         </Link>
       </div>
