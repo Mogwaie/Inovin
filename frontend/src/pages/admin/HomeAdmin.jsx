e266012d520b5f8dfb7e48faedd19e8a8a079f5e
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-07-20 22:34:43 +0200

    Adding all useful link for admin, admin and user routes

--- frontend/src/pages/admin/HomeAdmin.jsx
+++ frontend/src/pages/admin/HomeAdmin.jsx
@@ -7,23 +7,37 @@ function HomeAdmin() {
       <img className="profileImage" src={profilePicture} alt="Profile" />
       <div className="link-list-home-admin">
         <ul>
-          <Link to="/admin/workshop">
-            <li>Atelier de Création</li>
-          </Link>
+          <h2>Partie admin</h2>
           <Link to="/admin/user-list">
             <li>Membres</li>
-          </Link>
-          <Link to="/admin/degustation-profil">
-            <li>Profils de Dégustation</li>
           </Link>
           <Link to="/admin/wine-list">
             <li>Fiches de Vins</li>
           </Link>
+          <Link to="degustation-profil-list">
+            <li>Profils de Dégustation</li>
+          </Link>
+          <Link to="/admin/degustation">
+            <li>Fiche de dégustation</li>
+          </Link>
+          <Link to="/admin/workshop">
+            <li>Atelier de Création</li>
+          </Link>
+        </ul>
+
+        <ul>
+          <h2>Partie utilisateur</h2>
+          <Link to="/degustation">
+            <li>Fiche de dégustation</li>
+          </Link>
+          <Link to="/workshop">
+            <li>Atelier de Création</li>
+          </Link>
+          <Link to="wine-selection">
+            <li>Sélection des vins</li>
+          </Link>
         </ul>
       </div>
-      <a href="/login">
-        <p className="link-deconnection-admin">Déconnexion</p>
-      </a>
     </div>
   );
 }
